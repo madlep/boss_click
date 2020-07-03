5b46b166d5bfe7783c5f2b42f95c0b1e8510f186
Julian Doherty (madlep) <madlep@madlep.com>
2020-07-03 16:21:44 +1000

    set up secrets as env vars

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,6 @@ import Config
 config :boss_click, BossClickWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: nil, port: 443]
+  url: [host: "bossclick.madlep.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  live_view: [signing_salt: System.get_env("LIVE_VIEW_SIGNING_SALT")]
