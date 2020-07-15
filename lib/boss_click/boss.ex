307b17b39cfa830f456ba64c77f6ec1cbde802ea
Julian Doherty (madlep) <madlep@madlep.com>
2020-07-15 19:33:04 +1000

    alias Boss submodules

--- lib/boss_click/boss.ex
+++ lib/boss_click/boss.ex
@@ -1,26 +1,31 @@
 defmodule BossClick.Boss do
   @moduledoc false
 
+  alias BossClick.Boss.BossServer
+  alias BossClick.Boss.BossState
+
+  @type state() :: BossState.t()
+
   @spec start_link(max_health :: integer, GenServer.name()) :: {:ok, pid()}
-  def start_link(max_health, name \\ BossClick.Boss.BossServer) do
-    GenServer.start_link(BossClick.Boss.BossServer, max_health, name: name)
+  def start_link(max_health, name \\ BossServer) do
+    GenServer.start_link(BossServer, max_health, name: name)
   end
 
   @spec child_spec(max_health :: integer) :: Supervisor.child_spec()
   def child_spec(max_health) do
     %{
-      id: BossClick.Boss.BossServer,
+      id: BossServer,
       start: {BossClick.Boss, :start_link, [max_health]}
     }
   end
 
-  @spec current_state(GenServer.name()) :: BossClick.Boss.BossState.t()
-  def current_state(server \\ BossClick.Boss.BossServer) do
+  @spec current_state(GenServer.name()) :: state()
+  def current_state(server \\ BossServer) do
     GenServer.call(server, :current_state)
   end
 
   @spec damage(integer(), GenServer.name()) :: :ok
-  def damage(amount, server \\ BossClick.Boss.BossServer) do
+  def damage(amount, server \\ BossServer) do
     GenServer.cast(server, {:damage, amount})
   end
 end
